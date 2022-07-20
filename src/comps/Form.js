12abfa9f88ca9c1c17818b5974a8cc1cc2eace4a
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-20 21:11:13 +0530

    Update Form.js

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -47,7 +47,7 @@ const Form = () => {
     formData.append("schedule", data.scdate);
     try {
       const res = await axios.post(
-        "https://eventbackend.vercel.app/events",
+        "http://localhost:3030/events",
         formData
       );
       console.log(res);
